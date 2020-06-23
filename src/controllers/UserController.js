6abf8d88aec8a3a967c7f24240bbf64f24182ddf
Glauber Brack <glauber@brack.com.br>
2020-06-23 12:56:54 -0300

    insert users controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,35 @@
 const knex = require('../database')
+const { update } = require('../database')
 
 module.exports = {
   async index (req, res) {
     const results = await knex('users')
 
     return res.json(results)
+  },
+
+  async store(req, res, next) {
+    try {
+      const {username} = req.body
+
+      await knex('users').insert({
+        username
+      })
+
+      return res.status(201).send()
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  async update(req, res, next){
+    try {
+      const {username} = req.body
+      //59min
+      await knex('users')
+      .update({username})
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,15 @@ const express = require('express')
 const routes = require('./routes')
 
 const app = express()
+
+app.use(express.json())
 app.use(routes)
+
+//middleware
+app.use((error, req, res, netx) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 
 app.listen(3333, () => console.log('Server is running'))
